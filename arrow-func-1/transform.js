fbc888500c4b2601962deca0620a785fd9495059
Saurav Sahu <sauravsahu.12@live.com>
2018-01-20 11:21:17 +0530

    arrow-functions-1 - Fix output format

--- arrow-func-1/transform.js
+++ arrow-func-1/transform.js
@@ -1,12 +1,15 @@
 let transform = (names) => {
 
+    if (names.length == 0) return '';
+
     let firstCharacters = names.map(name => name[0]);
     let concatenated = firstCharacters.reduce((accumulator, currentValue) => {
         accumulator += currentValue;
         return accumulator;
     }, '');
 
-    return concatenated;
+    return `[${names.toString()}] becomes "${concatenated}"`;
 }
 
 module.exports = transform;
+console.log(transform(process.argv.slice(2)));
--- arrow-func-1/transform.test.js
+++ arrow-func-1/transform.test.js
@@ -5,5 +5,5 @@ test('Should return empty string for input as an empty array: ', () => {
 });
 
 test('Should return "Haf" for input as "Hello arrow functions" :', () => {
-    expect(transform(['Hello', 'arrow', 'functions'])).toBe('Haf');
+    expect(transform(['Hello', 'arrow', 'functions'])).toBe('[Hello,arrow,functions] becomes "Haf"');
 });
