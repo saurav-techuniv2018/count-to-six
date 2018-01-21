9692e539deb0c5ae1571e34c658fb88adcb5486a
Saurav Sahu <sauravsahu.12@live.com>
2018-01-21 16:44:49 +0530

    rest - add solution and test cases
    rest - add solution and test cases

---
+++ rest/average.js
@@ -0,0 +1,8 @@
+module.exports = function average(...numbers) {
+	if (numbers.length === 0) return 0;
+
+	let sum = numbers.reduce((accumulator, currentNumber) =>
+		accumulator + currentNumber, 0);
+
+	return sum / numbers.length;
+}
